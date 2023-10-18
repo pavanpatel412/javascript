00be892f05fb7d2afa0eda7066bf0775e5bbe558
pavanpatel412 <144308778+pavanpatel412@users.noreply.github.com>
2023-10-18 13:11:54 +0530

    Add files via upload

---
+++ booleanof.js
@@ -0,0 +1,26 @@
+let pavanisgoodboy = true
+ if (pavanisgoodboy){
+    console.log("above condition is satisfy")
+}
+else 
+{
+    console.log("the above condition is not satisfy")
+}
+let pavanisbadboy = false
+if(pavanisbadboy){
+    console.log("the above conditionis satisfy")
+}
+else {
+    console.log("condition is not satisfy")
+}
+
+
+let x = 4
+let y = 7
+if(x,y){
+    console.log(x>y)
+}
+else {
+    console.log("bvgvy")
+}
+
---
+++ if and else conditions.js
@@ -0,0 +1,64 @@
+var  p = "pavan"
+let v = 23.78
+const s = 78
+const r = 67
+const  x = s+r
+
+
+
+
+console.log("the type of a:",typeof(a))
+console.log("type of e:",typeof(e))
+console.log("type of var a is :",typeof(p))
+console.log("the value variable x is:",(x))
+console.log("the type of const x is:",typeof(x))
+//program using if else statements //
+a=200
+b=30
+if (a<b)
+{
+    console.log("a is less than b");
+
+}
+else if (a>b)
+{
+    console.log("a is greater than b");
+
+}
+else
+{
+    console.log("a is equal too b")
+}
+//program with 3 variables using && operater and == and +//
+var c = 20
+var d = 20
+var e = 40
+if (c<d && d<e)
+{
+    console.log(" c and d less then e");
+}
+else if (e>c && e>d)
+{
+    console.log("c and d is greater than e");
+}
+else if ( c+d == e )
+{
+console.log("c+d = 40 is correct");
+}
+else 
+
+{
+
+    console.log("both statements are not correct");
+}
+//using * OPERATOR
+i = 5
+j = 6
+k = 30
+if (i*j == k){
+    console.log("the above condition is true");
+}
+else 
+{
+    console.log("the above condition is false");
+}
